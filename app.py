6915114433d80099683bc6e9771c8c5bd75a1712
abhijit250692 <abhijit250692@gmail.com>
2026-06-05 23:26:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,13 +8,21 @@ scaler = joblib.load('scaler.pkl')
 
 sex_map ={'Female': 0, 'Male': 1}
 smoker_map = {'No': 0, 'Yes': 1}
-region_map = {'NorthEast': 0, 'NorthWest': 1, 'SouthEast': 2, 'SouthWest': 3}
+regions = ['NorthEast', 'NorthWest', 'SouthEast', 'SouthWest']
 
 image_url = "https://miro.medium.com/v2/0*ZfFdinuIaBOVxWwp.jpg"
 
+def prepare_input(age, bmi, children, sex, smoker, region):
+    region_NorthWest = 1 if region == 'NorthWest' else 0
+    region_SouthEast = 1 if region == 'SouthEast' else 0
+    region_SouthWest = 1 if region == 'SouthWest' else 0
+
+    input_data = np.array([[np.float64(age), bmi, np.log1p(np.float64(children)), sex, smoker, region_NorthWest, region_SouthEast, region_SouthWest]])
+    input_data_scaled = scaler.transform(input_data)
+    return input_data_scaled
 
 def main():
-    st.title("Income Prediction", text_alignment="center")
+    st.title("Insurance Charges Prediction", text_alignment="center")
     st.markdown(
         f'<img src="{image_url}" style="width:800px; height:350px; object-fit:cover;">',
         unsafe_allow_html=True
@@ -23,23 +31,20 @@ def main():
     with st.form("input_form"):
         col1, col2 = st.columns(2)
         with col1:
-            age = st.slider('Age', min_value=15.0, max_value=65.0, value=30.0, step=1.0)
+            age = st.slider('Age', min_value=15, max_value=65, value=30, step=1)
             sex = st.radio("Sex", options=list(sex_map.keys()))
             bmi = st.number_input('BMI', min_value=15.0, max_value=50.0, value=30.0, step=0.1)
         with col2:
-            children = st.selectbox('Dependent Children', options=[0.0, 1.0, 2.0, 3.0, 4.0, 5.0], index=0)
+            children = st.selectbox('Dependent Children', options=[0, 1, 2, 3, 4, 5], index=0)
             smoker = st.radio("Smoker", options=list(smoker_map.keys()))
-            region = st.selectbox('Region', options=list(region_map.keys()))            
+            region = st.selectbox('Region', options=regions, index=0)            
         lcol1, lcol2, lcol3 = st.columns([2, 1, 2])
         with lcol2:
             submitted = st.form_submit_button("Predict")
     if submitted:
-        input_data = np.array([[age, sex_map[sex], bmi, np.log1p(children), smoker_map[smoker], region_map[region]]])
-        input_data_scaled = scaler.transform(input_data)
+        input_data_scaled = prepare_input(age, bmi, children, sex_map[sex], smoker_map[smoker], region)
         predicted_charges = model.predict(input_data_scaled)[0]
         print(f"Predicted Insurance Charges: ${predicted_charges:,.2f}")
-        # 5. INVERSE TRANSFORM THE TARGET: Convert logged decimals back to real values
-        predicted_charges = np.expm1(predicted_charges)
         st.success(f"Predicted Insurance Charges: ${predicted_charges:,.2f}")
 
 if __name__ == "__main__":
